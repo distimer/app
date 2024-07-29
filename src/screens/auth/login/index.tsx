3977fd931b2160b327e34c52e7d869d91aaed5fa
noViceMin <admin@isamin.kr>
2024-07-29 11:42:43 +0900

    feat: apple login

--- src/screens/auth/login/index.tsx
+++ src/screens/auth/login/index.tsx
@@ -1,9 +1,45 @@
+import {
+  AndroidError,
+  AppleError,
+  appleAuth,
+  appleAuthAndroid,
+} from "@invertase/react-native-apple-authentication";
 import { Icon, VStack } from "components/common";
 import { LoginButton } from "components/features/auth";
 import { useTheme } from "contexts/theme";
+import { Platform } from "react-native";
 
 const Login: React.FC = () => {
   const { colors, styles } = useTheme();
+
+  const appleLogin = async () => {
+    if (Platform.OS === "ios") {
+      try {
+        const appleAuthRequestResponse = await appleAuth.performRequest({
+          requestedOperation: appleAuth.Operation.LOGIN,
+          requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
+        });
+
+        if (appleAuthRequestResponse.identityToken) {
+          console.log(appleAuthRequestResponse.identityToken);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    } else if (Platform.OS === "android") {
+      appleAuthAndroid.configure({
+        clientId: "api.distimer.com",
+        redirectUri: "https://api.distimer.com/auth/callback/apple",
+        scope: appleAuthAndroid.Scope.ALL,
+        responseType: appleAuthAndroid.ResponseType.ID_TOKEN,
+      });
+      const response = await appleAuthAndroid.signIn();
+
+      if (response.id_token) {
+        console.log(response.id_token);
+      }
+    }
+  };
 
   return (
     <VStack
@@ -18,7 +54,7 @@ const Login: React.FC = () => {
         <Icon name="OnboardingLogoIcon" fill={colors.gray[1000]} />
       </VStack>
       <VStack gap={400}>
-        <LoginButton type="apple" />
+        <LoginButton type="apple" onPress={appleLogin} />
         <LoginButton type="google" />
       </VStack>
     </VStack>
