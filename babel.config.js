beb5fb9ea868c9d45f250f08fd2c81a0d48e643a
noViceMin <admin@isamin.kr>
2024-07-29 09:04:56 +0900

    init: remove garbage

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [".js", ".ts", ".tsx"],
+      },
+    ],
+  ],
 };
