607a822a425421a01c57edcc757328dc6acc2e27
noViceMin <admin@isamin.kr>
2024-08-23 14:41:12 +0900

    feat: onesignal

--- env.ts
+++ env.ts
@@ -1,6 +1,7 @@
 const ENV = {
   googleClientId:
     "176360556447-9c99g5nu9vcj5df4eea8m16g1nj5f2p9.apps.googleusercontent.com",
+  oneSignalAppId: "1271ceba-9e5f-4ae0-a62c-385d750f3ef7",
 };
 
 export { ENV };
---
+++ ios/OneSignalWidget/OneSignalWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  OneSignalWidgetBundle.swift
+//  OneSignalWidget
+//
+//  Created by noViceMin on 8/23/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct OneSignalWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        OneSignalWidgetLiveActivity()
+    }
+}
