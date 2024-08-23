7fc21689e3c3def341ac60f4b6aac1b4d704a278
noViceMin <admin@isamin.kr>
2024-08-23 21:11:45 +0900

    feat: ios live activity ui

---
+++ ios/LiveActivity/LiveActivityAttributes.swift
@@ -0,0 +1,18 @@
+//
+//  LiveActivityAttributes.swift
+//  distimer
+//
+//  Created by noViceMin on 8/23/24.
+//
+
+import Foundation
+import ActivityKit
+
+struct LiveActivityAttributes: ActivityAttributes {
+  public struct ContentState: Codable, Hashable {
+    var startedAt: Int
+    var subject: String
+    var color: String
+    var content: String
+  }
+}
---
+++ ios/LiveActivity/LiveActivityBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LiveActivityBundle.swift
+//  LiveActivity
+//
+//  Created by noViceMin on 8/23/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LiveActivityBundle: WidgetBundle {
+  var body: some Widget {
+    LiveActivity()
+  }
+}
